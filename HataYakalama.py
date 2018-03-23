993f69bc19ba87f3cdd1a11bcec6c673ad731ea9
Emre Çağlar YILDIZ <emrecaglaryildiz@gmail.com>
2018-03-23 15:55:58 +0300

    HataYakalama.py
    HataYakalama.py

---
+++ HataYakalama.py
@@ -0,0 +1,8 @@
+try:
+    a=int(7)
+    print("program burada")
+
+except: #yanına alınan hangi hatada çalışacağı yazlılabilir. ör: except ValueError gibi.
+    print("hata oluştu")
+
+print("bloklar sona erdi.")
