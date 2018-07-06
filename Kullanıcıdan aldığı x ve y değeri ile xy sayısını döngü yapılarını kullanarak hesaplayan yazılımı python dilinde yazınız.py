1563304689298ad9acc2b4d2b6125e80ac7edf8b
Emre Çağlar YILDIZ <emrecaglaryildiz@gmail.com>
2018-07-06 15:53:30 +0300

    Add files via upload

--- Kullanıcıdan aldığı x ve y değeri ile xy sayısını döngü yapılarını kullanarak hesaplayan yazılımı python dilinde yazınız.py
+++ Kullanıcıdan aldığı x ve y değeri ile xy sayısını döngü yapılarını kullanarak hesaplayan yazılımı python dilinde yazınız.py
@@ -1,7 +1,10 @@
 #Kullanıcıdan aldığı x ve y değeri ile x üzeri y sayısını döngü yapılarını kullanarak hesaplayan yazılımı python dilinde yazınız.
 
+# %%
 x=int(input("birinci sayıyı giriniz: "))
 y=int(input("üst olarak kullanılacak sayıyı giriniz: "))
 
 us=x**y
 print(us)
+
+# %%
