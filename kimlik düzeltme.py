870bcf4be8c050365b6fa17b76760c59d9464886
Emre Çağlar YILDIZ <emrecaglaryildiz@gmail.com>
2018-10-21 11:34:16 +0300

    Add files via upload

---
+++ kimlik düzeltme.py
@@ -0,0 +1,3 @@
+a=input("16 haneli kimlik kodunu giriniz ?\n")
+print(a[14]+a[15]+a[12]+a[13]+a[10]+a[11]+a[8]+a[9]+a[6]+a[7]+a[4]+a[5]+a[2]+a[3]+a[0]+a[1])
+input()
---
+++ sipariş otomatı.py
@@ -0,0 +1,35 @@
+﻿#!/usr/bin/env python3.0
+
+
+kim=input("senin adın ne\n")
+print("Merhaba "+ kim +" sisteme hoşgeldiniz !")
+
+a="çık"
+b="sipariş vereceğim"
+c="iş başvurusu yapacağım"
+
+soru=input("size nasıl yardımcı olabilirim \n" +a+" ? - 1 \n" +b+" ? - 2 \n"+c+" ? - 3 \n")
+
+if(soru=="1"):
+    exit()
+elif(soru=="2"):
+    a="Yiyecek "
+    b="İçecek "
+    tercih=input("Ne arzu edersiniz" +" \n"+"1- "+a+" \n"+"2- "+b+" \n")
+    if(tercih=="2"):
+                b1="kola"
+                b2="fanta"
+                b3="sprite"
+                seçim=input("1- "+b1+" \n"+"2- "+b2+" \n"+"3- "+b3+" \n")
+    if(seçim=="1"):
+                            print("siparişiniz alınmıştır.")
+                            input()
+    elif(seçim=="2"):
+                            print("siparişiniz alınmıştır.")
+                            input()
+    elif(seçim=="3"):
+                            print("siparişiniz alınmıştır.")
+                            input()
+elif(soru=="3"):
+    print("devam edecek menü yoktur, yazılımı geliştiriniz :)")
+
